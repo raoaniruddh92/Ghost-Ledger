3afb8412205a128e56f785259e5b862458a547dd
Aniruddh Rao <raoaniruddh92@gmail.com>
2025-12-07 23:26:08 +0530

    Css fix

--- src/Challenges/Challenges.js
+++ src/Challenges/Challenges.js
@@ -5,36 +5,38 @@ function Challenges() {
     <div className="terminal-wrapper">
       <h1 className="terminal-header">GhostLedger</h1>
 
-      <div className="terminal-card">
-        <h2>Challenge 1</h2>
-        <p>Solve the first vulnerability and complete the task.</p>
-        <Link to="/challenge1" className="cy-button">
-          ▶ Start Challenge 1
-        </Link>
-      </div>
+      <div className="challenge-list">
+        <div className="terminal-card">
+          <h2>Challenge 1</h2>
+          <p>Solve the first vulnerability and complete the task.</p>
+          <Link to="/challenge1" className="cy-button">
+            ▶ Start Challenge 1
+          </Link>
+        </div>
 
-      <div className="terminal-card">
-        <h2>Challenge 2</h2>
-        <p>Interact with the smart contract to complete the level.</p>
-        <Link to="/challenge2" className="cy-button">
-          ▶ Start Challenge 2
-        </Link>
-      </div>
+        <div className="terminal-card">
+          <h2>Challenge 2</h2>
+          <p>Interact with the smart contract to complete the level.</p>
+          <Link to="/challenge2" className="cy-button">
+            ▶ Start Challenge 2
+          </Link>
+        </div>
 
-      <div className="terminal-card">
-        <h2>Challenge 3</h2>
-        <p>Guess the random number</p>
-        <Link to="/challenge3" className="cy-button">
-          ▶ Start Challenge 3
-        </Link>
-      </div>
+        <div className="terminal-card">
+          <h2>Challenge 3</h2>
+          <p>Guess the random number</p>
+          <Link to="/challenge3" className="cy-button">
+            ▶ Start Challenge 3
+          </Link>
+        </div>
 
-      <div className="terminal-card">
-        <h2>Challenge 4</h2>
-        <p>Guess the random number pt-2</p>
-        <Link to="/challenge4" className="cy-button">
-          ▶ Start Challenge 3
-        </Link>
+        <div className="terminal-card">
+          <h2>Challenge 4</h2>
+          <p>Guess the random number pt-2</p>
+          <Link to="/challenge4" className="cy-button">
+            ▶ Start Challenge 4
+          </Link>
+        </div>
       </div>
     </div>
   );
