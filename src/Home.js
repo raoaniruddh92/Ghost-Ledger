b04982bd397cb1043cf3051797fd10ff14c1d0ea
Aniruddh Rao <raoaniruddh92@gmail.com>
2025-12-08 18:34:48 +0530

    Wallet connect fix

--- src/Home.js
+++ src/Home.js
@@ -10,7 +10,7 @@ function Home() {
       <p className="subtitle">Blockchain security challenges-A collection of all the challenges on different sites you can't play anymore</p>
 
 
-      <p className="subtitle">I made this because capture the ether has been depracated for a while now ;( </p>
+      <p className="subtitle">Some problems could not be ported because they rely on vulnerabilities that only existed in older versions of Solidity. Some liberty has been taken where the syntax and other compiler features are no longer compatible. </p>
       <p className="subtitle" >This project is fully open source so feel free to contribute</p>
 
       <Link to="/challenges">
