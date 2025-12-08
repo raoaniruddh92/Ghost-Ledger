0b3709343f5be4d4936bdeced193ed9cdb31cbcd
Aniruddh Rao <raoaniruddh92@gmail.com>
2025-12-08 18:16:26 +0530

    bug fixes

--- src/Challenges/Challenge5helpers/interact.js
+++ src/Challenges/Challenge5helpers/interact.js
@@ -1,4 +1,4 @@
-import { Challenges5abi as abi, Challenges5bytecode as bytecode } from "../abi";
+import { Challenges5abi as abi } from "../abi";
 import { BrowserProvider } from "ethers";
 const { ethers } = require("ethers");
 
@@ -11,7 +11,7 @@ export async function  interact(address){
     const signer = await provider.getSigner();  
     const contract = new ethers.Contract(address, abi, signer);    
     const result = await contract.isComplete();
-    if (result==true){
+    if (result===true){
         return true;
     }else{
         return false;
